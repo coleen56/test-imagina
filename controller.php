e1818f2f7a86d10a5b4859996e6a2dd72ccc3b8e
CONTE Coleen <coleen.conte@auray-quiberon.fr>
2025-06-13 16:17:25 +0200

    presque ok

--- controller.php
+++ controller.php
@@ -1,6 +1,8 @@
 <?php
-
-function csvToArray($fichier) {
+require_once 'modele/Cellule.php';
+use modele\Cellule;
+// fonction qui prend en paramètre un lien de fichier CSV et convertit son contenu en array
+function csvToArray(String $fichier) {
     $ret = [];
     if (($handle = fopen($fichier, "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
@@ -12,7 +14,52 @@ function csvToArray($fichier) {
 }
 
 $fichier = "champs-de-menhirs.csv";
-
+// conversion données du fichier csv en array
 $champs = csvToArray($fichier);
+// cellule du départ : coin inférieur gauche
+$start = new Cellule(9,0);
 
+// fonction qui prend en paramètre une cellule et retourne ses voisines vides sous forme d'un array (hors voisines en diagonales)
+function getCasesVoisinesVides(Cellule $cell) {
+    global $champs;
+    $x = $cell->getX();
+    $y = $cell->getY();
+    // valeur d'une case à -1 permet de la déclarer "visitée) donc non vide et pas un menhir
+    $champs[$x][$y] = -1;
+    $voisinesVides = [];
+    // balayage vertical des cases voisines
+    for($i = $x-1; $i <= $x+1; $i++) {
+        if(array_key_exists($i, $champs) && $champs[$i][$y] == 0 && $x != $i) {
+            $voisinesVides[] = new Cellule($i, $y);
+        }
+    }
+    // balayage horizontal des cases voisines
+    for($i = $y-1; $i <= $y+1; $i++) {
+        if(array_key_exists($i, $champs[$x]) && $champs[$x][$i] == 0 && $y != $i) {
+            $voisinesVides[] = new Cellule($x, $i);
+        }
+    }
+    return $voisinesVides;
+}
+
+
+function getNiveauPropagation() {
+    global $start;
+    $cellulesTouchees[] = $start;
+    echo "<pre>";
+    $compteur = 1;
+    while(!empty($cellulesTouchees)) {
+        $currentCells = $cellulesTouchees;
+        $cellulesTouchees = [];
+        foreach($currentCells as $curCell) {
+            // echo "Cellules touchées au tour n°" . $compteur . " par la cellule " . $curCell->toString() . "\n";
+            $cellulesTouchees =array_merge($cellulesTouchees, getCasesVoisinesVides($curCell));
+            // print_r($cellulesTouchees);
+        }
+        $compteur++;
+    }
+    return $compteur;
+}
+
+echo(getNiveauPropagation());
 ?>
